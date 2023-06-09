69b8a0a7fcded3661f51b81d773da439549b57a1
jayassk <135097694+jayassk@users.noreply.github.com>
2023-06-09 11:30:45 +0530

    Add files via upload

---
+++ AssignmentLUG3.py
@@ -0,0 +1,10 @@
+a=[2,3,4,55,56,78,75,69,66,101,100]
+even,odd=0,0
+for i in a:
+     if i%2==0:
+          even +=1
+     else:
+          odd +=1
+print("Even numbers in the list",even)    
+print("Odd numbers in the list",odd)      
+
